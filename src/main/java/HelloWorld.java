208c51f81f3b6a2719b11fa5137061d4d0dca014
Mikael Lof <mickes511@hotmail.com>
2012-04-10 14:34:04 +0200

    added hello world

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,33 +1,24 @@
-/**
- * Created with IntelliJ IDEA.
- * User: mikael.lof
- * Date: 2012-04-10
- * Time: 14:06
- * To change this template use File | Settings | File Templates.
- */
-public class HelloWorld {
-    import java.io.IOException;
-    import javax.servlet.ServletException;
-    import javax.servlet.http.*;
-    import org.eclipse.jetty.server.Server;
-    import org.eclipse.jetty.servlet.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
 
-    public class HelloWorld extends HttpServlet {
+public class HelloWorld extends HttpServlet {
 
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-                throws ServletException, IOException {
-            resp.getWriter().print("Hello Mike!\n");
-        }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Java!\n");
+    }
 
-        public static void main(String[] args) throws Exception{
-            Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-            context.setContextPath("/");
-            server.setHandler(context);
-            context.addServlet(new ServletHolder(new HelloWorld()),"/*");
-            server.start();
-            server.join();
-        }
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+        server.start();
+        server.join();
     }
 }
