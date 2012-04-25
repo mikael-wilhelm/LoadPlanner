41a0714d4073c5b7620066bfdb7c4c7743408700
Mikael Lof <mickes511@hotmail.com>
2012-04-25 09:38:02 +0200

    test

--- src/main/java/servlet/StartUpServlet.java
+++ src/main/java/servlet/StartUpServlet.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.core.metadata.PostgresCallMetaDataProvider;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -16,10 +18,12 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class StartUpServlet extends HttpServlet {
-    public StartUpServlet() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("SHARED_DATABASE_URL"));
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res)  {
+
         try {
+            URI dbUri = new URI(System.getenv("SHARED_DATABASE_URL"));
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
             Driver driver = new org.postgresql.Driver();
             SimpleDriverDataSource dataSource = new SimpleDriverDataSource(driver,dbUrl );
             dataSource.setUsername(dbUri.getUserInfo().split(":")[0]);
